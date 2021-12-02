5d3cc1dcf7c68d995e4cbd080d345d4dcc98e81d
Justin Derus <77174869+Justin-Derus@users.noreply.github.com>
2021-12-02 16:56:12 -0600

    Create empinv.php

---
+++ empinv.php
@@ -0,0 +1,48 @@
+<html><head><title>Employee View</title></head>
+    <body>
+        <?php
+        include("secret.php");
+        include("functions.php");
+        // username
+        // password
+        try{
+            $dsn = "mysql:host=courses;dbname=".$username;
+            $pdo = new PDO($dsn, $username, $password);
+        }
+         catch(PDOexception $e){
+            echo "Connection to database failed: " . $e->getMessage();
+	 }
+        print("LIST OF TOTAL ITEMS:\n");
+        $result1 = $pdo->query("SELECT * FROM Items;");
+        $rows = $result1->fetchAll(PDO::FETCH_ASSOC);
+        // draw the inventory table
+        draw_table($rows);
+      
+        print("LIST OF ITEMS IN STOCK:\n");
+        $result2 = $pdo->query("SELECT * FROM Instock;");
+        $Inrows = $result2->fetchAll(PDO::FETCH_ASSOC);
+        // draw the inventory table
+        draw_table($Inrows);
+      
+        print("LIST OF ITEMS OUT OF STOCK:\n");
+        $result3 = $pdo->query("SELECT * FROM Outstock;");
+        $Outrows = $result3->fetchAll(PDO::FETCH_ASSOC);
+
+        // draw the inventory table
+        draw_table($Outrows);
+        
+	}
+	echo "<table cellpadding=30>";
+		echo "<tr>";
+			echo "<th>";
+				echo '<a href="home.html">';
+					echo "<h2>";
+						echo "Return to homepage";
+					echo "</h2>";
+				echo "</a>";
+			echo "</th>";
+		echo "</tr>";
+	echo "</table>";
+?>
+    </body>
+</html>
