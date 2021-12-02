dab41931e9fb3b83314847a4fb408a0399a49997
Justin Derus <77174869+Justin-Derus@users.noreply.github.com>
2021-12-02 16:17:52 -0600

    Update ShoppingCart.php
    Added hyper links to continue shopping and an extra hyper link to let user go to checkout page (doesnt exist yet so it links back to homepage)

--- ShoppingCart.php
+++ ShoppingCart.php
@@ -1,4 +1,4 @@
-<html><head><title>Items In Stock</title></head>
+<html><head><title>Shopping Cart</title></head>
     <body>
 <?php
 	include("secret.php");
@@ -13,10 +13,31 @@
         print("Your Shopping Cart:\n");
         $result = $pdo->query("SELECT * FROM Cart;");
         $rows = $result->fetchAll(PDO::FETCH_ASSOC);
-        draw_table($rows);
+	draw_table($rows);
 	echo "<br/>";
 	print("Would you like to continue shopping or checkout? ");
 	echo "<br/>";
-            ?>
+	echo "<table cellpadding=30>";
+		echo "<tr>";
+			echo "<th>";
+				echo '<a href="inventory.php">';
+					echo "<h2>";
+						echo "Continue Shopping";
+					echo "</h2>";
+				echo "</a>";
+			echo "</th>";
+			echo "<th>";
+				echo "OR";
+			echo "</th>";
+			echo "<th>";
+				echo '<a href="home.html">';
+					echo "<h2>";
+						echo "Checkout";
+					echo "</h2>";
+				echo "</a>";
+			echo "</th>";
+		echo "</tr>";
+	echo "</table>";
+?>
     </body>
 </html>
