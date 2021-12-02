8f7bfb3e5bd4341e6d59259abc50356cb2222e66
Justin Derus <77174869+Justin-Derus@users.noreply.github.com>
2021-12-01 19:26:04 -0600

    Update sampleData.sql

--- sampleData.sql
+++ sampleData.sql
@@ -45,19 +45,23 @@ CREATE TABLE IF NOT EXISTS Contains (
 
 CREATE TABLE IF NOT EXISTS Instock (
   `IID` CHAR(4) NOT NULL,
+  `QTY` INT NOT NULL,
+  `NAME` VARCHAR(255) NOT NULL,
+  `COST` DECIMAL(10,2) NOT NULL,
   `ITEMTYPE` VARCHAR(255) NOT NULL,
-  FOREIGN KEY (`IID`) REFERENCES `Items`(`IID`),
   PRIMARY KEY (`IID`)
 );
 
 CREATE TABLE IF NOT EXISTS Outstock (
   `IID` CHAR(4) NOT NULL,
+  `QTY` INT NOT NULL,
+  `NAME` VARCHAR(255) NOT NULL,
+  `COST` DECIMAL(10,2) NOT NULL,
   `LOCATION` VARCHAR(255) NOT NULL,
   `ITEMTYPE` VARCHAR(255) NOT NULL,
   FOREIGN KEY (`IID`) REFERENCES `Items`(`IID`),
   PRIMARY KEY (`IID`)
 );
-
 
 CREATE TABLE IF NOT EXISTS Checkout (
   `SUM` INT NOT NULL,
@@ -67,8 +71,6 @@ CREATE TABLE IF NOT EXISTS Checkout (
   FOREIGN KEY (`IID`) REFERENCES `Items`(`IID`),
   PRIMARY KEY (`NUM`)
 );
-
-
 
 CREATE TABLE IF NOT EXISTS Purchase (
   `IID` CHAR(4) NOT NULL,
@@ -109,28 +111,27 @@ CREATE TABLE IF NOT EXISTS View (
 );
 
 INSERT INTO Items (`IID`,`QTY`,`NAME`,`COST`) VALUES
-  ('P001','1000','Apple Sauce','0.99'),
-  ('P002','70','Ribeye Steak','55.68'),
-  ('P003','0','Cereal','9.99'),
-  ('P004','35','Milk','1.89'),
-  ('P005','40','Eggs','2.35'),
-  ('P006','15','Jelly','3.98'),
-  ('P007','65','Bread','2.75'),
-  ('P008','0','Pretzels','8.76'),
-  ('P009','23','Peanut Butter','4.19'),
-  ('P010','50','Chicken','3.99'),
-  ('P011','0','Olive Oil','9.99'),
-  ('P012','100','Salt','0.59'),
-  ('P013','82','Hot Sauce','12.41'),
-  ('P014','16','Rice','25.99'),
-  ('P015','0','Potato Chips','8.75'),
-  ('P016','200','Pepsi','4.98'),
-  ('P017','0','Butter','1.15'),
-  ('P018','60','Cheese','6.59'),
-  ('P019','80','Honey','12.97'),
-  ('P020','0','Sugar','2.39'),
-  ('P021','25','Salmon','31.94');
-
+  ('P001',1000,'Apple Sauce',0.99),
+  ('P002',70,'Ribeye Steak',55.68),
+  ('P003',10,'Cereal',9.99),
+  ('P004',35,'Milk',1.89),
+  ('P005',40,'Eggs',2.35),
+  ('P006',15,'Jelly',3.98),
+  ('P007',65,'Bread',2.75),
+  ('P008',10,'Pretzels',8.76),
+  ('P009',23,'Peanut Butter',4.19),
+  ('P010',50,'Chicken',3.99),
+  ('P011',10,'Olive Oil',9.99),
+  ('P012',100,'Salt',0.59),
+  ('P013',82,'Hot Sauce',12.41),
+  ('P014',16,'Rice',25.99),
+  ('P015',10,'Potato Chips',8.75),
+  ('P016',200,'Pepsi',4.98),
+  ('P017',10,'Butter',1.15),
+  ('P018',60,'Cheese',6.59),
+  ('P019',80,'Honey',12.97),
+  ('P020',10,'Sugar',2.39),
+  ('P021',25,'Salmon',31.94);
 
 INSERT INTO User (`UID`,`NAME`,`CC`,`ADDRESS`) VALUES
   ('C0001','John Smith','4382688703331714','815 Cherry st'),
@@ -149,35 +150,31 @@ INSERT INTO Ord (`ORDERNUM`,`TOTAL`) VALUES
   ('00006', '33.52'),
   ('00007', '51.88');
 
-
 INSERT INTO Worker (`WID`,`NAME`) VALUES
   ('1000', 'David Pawski'),
   ('1001', 'Juana Monte');
 
+INSERT INTO Instock (`IID`,`QTY`,`NAME`,`COST`,`ITEMTYPE`) VALUES
+  ('P001',100,'Apple Sauce',0.99,'IN STOCK'),
+  ('P003',5,'Cereal',9.99,'IN STOCK'),
+  ('P005',30,'Eggs',2.35,'IN STOCK'),
+  ('P007',30,'Bread',2.75,'IN STOCK'),
+  ('P009',10,'Peanut Butter',4.19,'IN STOCK'),
+  ('P011',5,'Olive Oil',9.99,'IN STOCK'),
+  ('P013',40,'Hot Sauce',12.41,'IN STOCK'),
+  ('P015',5,'Potato Chips',8.75,'IN STOCK'),
+  ('P017',5,'Butter',1.15,'IN STOCK'),
+  ('P019',30,'Honey',12.97,'IN STOCK'),
+  ('P021',12,'Salmon',31.94,'IN STOCK');
 
-
-
-INSERT INTO Instock (`IID`, `ITEMTYPE`) VALUES
-  (`P001`, `Dry Good`),
-  (`P002`, `Meat`),
-  (`P004`, `Cold Good`),
-  (`P005`, `Cold Good`),
-  (`P006`, `Dry Good`),
-  (`P007`, `Dry Good`),
-  (`P009`, `Dry Good`),
-  (`P010`, `Meat`),
-  (`P012`, `Spice`),
-  (`P013`, `Spice`),
-  (`P014`, `Dry Good`),
-  (`P016`, `Dry Good`),
-  (`P018`, `Cold Good`),
-  (`P019`, `Spice`),
-  (`P021`, `Meat`);
-
-INSERT INTO Outstock (`IID`,`LOCATION`, `ITEMTYPE`) VALUES
-  (`P003`, `General Mills`, `Dry Good`),
-  (`P008`, `Costco`, `Dry Good`),
-  (`P011`, `Italy`, `Spice`),
-  (`P015`, `Dekalb`, `Dry Good`),
-  (`P017`, `Dairy Farm`, `Cold Good`),
-  (`P020`, `Dekalb`, `Spice`);
+INSERT INTO Outstock (`IID`,`QTY`,`NAME`,`COST`,`LOCATION`,`ITEMTYPE`) VALUES
+  ('P002',70,'Ribeye Steak',55.68,'Chicago','OUT OF STOCK'),
+  ('P004',35,'Milk',1.89,'Dekalb','OUT OF STOCK'),
+  ('P006',15,'Jelly',3.98,'Oswego','OUT OF STOCK'),
+  ('P008',10,'Pretzels',8.76,'Dekalb','OUT OF STOCK'),
+  ('P010',50,'Chicken',3.99,'Saint Charles','OUT OF STOCK'),
+  ('P012',100,'Salt',.59,'Dekalb','OUT OF STOCK'),
+  ('P014',16,'Rice',25.99,'Oswego','OUT OF STOCK'),
+  ('P016',200,'Pepsi',4.98,'Batavia','OUT OF STOCK'),
+  ('P018',60,'Cheese',6.59,'Chicago','OUT OF STOCK'),
+  ('P020',10,'Sugar',2.39,'Chicago','OUT OF STOCK');
