8e41f62fc3fc9331222743d5ba962a2be615e11b
xxplunderxx <71054045+xxplunderxx@users.noreply.github.com>
2021-12-03 16:09:35 -0600

    Update Checkout.php
    Fullfilment

--- Checkout.php
+++ Checkout.php
@@ -7,9 +7,10 @@
             $dsn = "mysql:host=courses;dbname=".$username;
             $pdo = new PDO($dsn, $username, $password);
         }
-        catch(PDOexception $e){
+        catch(PDOexception $e)
+        {
             echo "Connection to database failed: " . $e->getMessage();
-	}
+	    }
 	
 	$sql = "SELECT TOTAL,_COUNT FROM Cart;";
 	$result = $pdo->query($sql);
@@ -20,7 +21,7 @@
 		$TOTAL+= $total * $count;
 	}
 
-        echo "<h1>";
+    echo "<h1>";
 	echo "CHECKOUT:";
 	echo "</h1>";
 	$result = $pdo->query("SELECT _NAME,_COUNT,TOTAL FROM Cart;");
@@ -31,17 +32,21 @@
 	draw_table_no_header($rows);
 	echo "<h3>";
 	echo "Cart Total: $" . $TOTAL;
+
 	checkout_form();
-        if(isset($_POST['_NAME']))
-        {
-            $name = $_POST['_NAME'];
-	    $addy = $_POST['ADDRESS'];
-	    $card = $_POST['CC'];
-            //working insert into User table	     
-	    $prepared2 = $pdo->prepare('INSERT INTO User(_NAME,CC,ADDRESS) VALUE(?,?,?);');
-	    $prepared2->execute(array($name,$card,$addy));
-	    echo "<meta http-equiv='refresh' content='0'>";
-	}
+
+    if(isset($_POST['_NAME']))
+    {
+        $name = $_POST['_NAME'];
+        $addy = $_POST['ADDRESS'];
+        $card = $_POST['CC'];
+
+        //working insert into User table	     
+        $prepared2 = $pdo->prepare('INSERT INTO User(_NAME,CC,ADDRESS) VALUE(?,?,?);');
+        $prepared2->execute(array($name,$card,$addy));
+        echo "<meta http-equiv='refresh' content='0'>";
+    }
+
 	// completly submit
 	echo "<br/>";
     	echo '<form method="POST">';
@@ -51,17 +56,20 @@
 
         	echo '<br/><input type="submit" value="Submit"/>';
 	echo "</form>";
+
 	 if(isset($_POST['Done']))
         {
-              
+        // insert into order
 	    $prepared2 = $pdo->prepare('INSERT INTO Ord(TOTAL) VALUE(?);');
 	    $prepared2->execute(array($TOTAL));
-	    $prepared2 = $pdo->prepare('INSERT INTO Fullfilment(TOTAL) VALUE(?);');
-	    $prepared2->execute(array($TOTAL));
+
+        // insert into fullfilment
+	    $prepared2 = $pdo->prepare('INSERT INTO Fullfilment(Status,QTY) VALUE(?,?,?);';
+	    $prepared2->execute(array("Pending",$TOTAL));
 
 	    echo "<meta http-equiv='refresh' content='0'>";
 	    //reset the shopping cart for the next customer
-            $result = $pdo->query("DELETE FROM Cart WHERE TOTAL > 0.00");
+        $result = $pdo->query("DELETE FROM Cart WHERE TOTAL > 0.00");
 	}
 	?>
     </body>
