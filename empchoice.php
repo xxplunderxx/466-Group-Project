ca65f192537fc6ed530831fc0215dc8ac5601f14
Justin Derus <77174869+Justin-Derus@users.noreply.github.com>
2021-12-03 16:09:11 -0600

    Update empchoice.php

--- empchoice.php
+++ empchoice.php
@@ -3,8 +3,6 @@
         <?php
         include("secret.php");
         include("functions.php");
-        // username
-        // password
         try{
             $dsn = "mysql:host=courses;dbname=".$username;
             $pdo = new PDO($dsn, $username, $password);
@@ -12,35 +10,38 @@
          catch(PDOexception $e){
             echo "Connection to database failed: " . $e->getMessage();
 	 }
-        print("LIST OF TOTAL ITEMS:\n");
-        $result1 = $pdo->query("SELECT * FROM Items;");
-        $rows = $result1->fetchAll(PDO::FETCH_ASSOC);
-        // draw the inventory table
-        draw_table($rows);
-      
-        print("LIST OF ITEMS IN STOCK:\n");
-        $result2 = $pdo->query("SELECT * FROM Instock;");
-        $Inrows = $result2->fetchAll(PDO::FETCH_ASSOC);
-        // draw the inventory table
-        draw_table($Inrows);
-      
-        print("LIST OF ITEMS OUT OF STOCK:\n");
-        $result3 = $pdo->query("SELECT * FROM Outstock;");
-        $Outrows = $result3->fetchAll(PDO::FETCH_ASSOC);
-
-        // draw the inventory table
-        draw_table($Outrows);
 	echo "<table cellpadding=30>";
 		echo "<tr>";
 			echo "<th>";
-				echo '<a href="home.html">';
+				echo '<a href="employeeinventory.php">';
 					echo "<h2>";
-						echo "Return to homepage";
+						echo "VIEW INVENTORY";
 					echo "</h2>";
 				echo "</a>";
 			echo "</th>";
+			echo "<th>";
+				echo "OR";
+			echo "</th>";
+			echo "<th>";
+				echo '<a href="empedit.php">';
+					echo "<h2>";
+						echo "EDIT INVENTORY";
+					echo "</h2>";
+				echo "</a>";
+			echo "</th>";
+			echo "<th>";
+			echo "OR";
+			echo "</th>";
+			echo "<th>";
+				echo '<a href="home.html">';
+					echo "<h2>";
+						echo "RETURN TO HOMEPAGE";
+					echo "</h2>";
+				echo "</a>";
+			echo "</th>";
+
 		echo "</tr>";
 	echo "</table>";
-?>
+	?>
     </body>
 </html>
