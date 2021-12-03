904fb41d1a66bc2f62c1c3ac783fb43835f15d28
Justin Derus <77174869+Justin-Derus@users.noreply.github.com>
2021-12-03 00:57:30 -0600

    Update Checkout.php
    working total count

--- Checkout.php
+++ Checkout.php
@@ -9,26 +9,33 @@
         }
         catch(PDOexception $e){
             echo "Connection to database failed: " . $e->getMessage();
-        }
-        echo "<h1>";
-        echo "CHECKOUT: ";
-        echo "</h1>";
-                checkout_form();
+	}
+	
+	$sql = "SELECT TOTAL,_COUNT FROM Cart;";
+	$result = $pdo->query($sql);
+	//FETCH BOTH NEEDED TO GET USABLE INDEX	
+	$rows = $result->fetchAll(PDO::FETCH_BOTH);
+	$TOTAL = 0.00;
+	foreach($rows as [$total,$count]){
+		$TOTAL+= $total * $count;
+	}
 
+        echo "<h1>";
+        echo "CHECKOUT TOTAL: ".$TOTAL;
+        echo "</h1>";
+        checkout_form();
         if(isset($_POST['_NAME']))
         {
-        // decalare varaiables
-        $name = $_POST['_NAME'];
+            $name = $_POST['_NAME'];
 	    $addy = $_POST['ADDRESS'];
 	    $card = $_POST['CC'];
            	     
-	    // insert new part
-            $prepared2 = $pdo->prepare('INSERT INTO User(_NAME,CC,ADDRESS) VALUE(?,?,?);');
+	    $prepared2 = $pdo->prepare('INSERT INTO User(_NAME,CC,ADDRESS) VALUE(?,?,?);');
 
-	    // execute sql query
 	    $prepared2->execute(array($name,$card,$addy));
 	    echo "<meta http-equiv='refresh' content='0'>";
-        }
-        ?>
+	}
+		
+	?>
     </body>
 </html>
