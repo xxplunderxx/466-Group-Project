db47dd1546c15c544479c4e5ff54ae3b8a92861c
Justin Derus <77174869+Justin-Derus@users.noreply.github.com>
2021-12-02 18:27:22 -0600

    Update Checkout.php

--- Checkout.php
+++ Checkout.php
@@ -4,7 +4,7 @@
         include("secret.php");
         include("functions.php");
         try{
-            $dsn = "mysql:host=courses;dbname=z1854210";
+            $dsn = "mysql:host=courses;dbname=".$username;
             $pdo = new PDO($dsn, $username, $password);
         }
         catch(PDOexception $e){
