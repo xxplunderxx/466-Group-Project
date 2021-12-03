e35f3e23b7c8e233d6cef3d57eb6721cf29a0768
Justin Derus <77174869+Justin-Derus@users.noreply.github.com>
2021-12-02 20:47:52 -0600

    Update ShoppingCart.php

--- ShoppingCart.php
+++ ShoppingCart.php
@@ -9,13 +9,43 @@
         }
         catch(PDOexception $e){
             echo "Connection to database failed: " . $e->getMessage();
-        }
-        print("Your Shopping Cart:\n");
+	}
+	echo "<h2>";
+	print("Your Shopping Cart:\n");
+	echo "</h2>";
         $result = $pdo->query("SELECT * FROM Cart;");
-        $rows = $result->fetchAll(PDO::FETCH_ASSOC);
+	$rows = $result->fetchAll(PDO::FETCH_ASSOC);
+	echo "<br/>";
 	draw_table($rows);
 	echo "<br/>";
-	print("Would you like to continue shopping or checkout? ");
+	cart_form($rows);
+	if (isset($_POST['ORDERNUM1']))
+        {
+            // decalare varaiables
+	    $item_id1 = $_POST['ORDERNUM1'];
+	    $COUNT = $_POST['COUNT'];
+           	     
+	    // insert new part
+            $prepared2 = $pdo->prepare('UPDATE Cart SET COUNT = ? WHERE ORDERNUM=?');
+
+	    // execute sql query
+	    $prepared2->execute(array($COUNT,$item_id1));
+	    //echo "<meta http-equiv='refresh' content='0'>";
+	    
+
+	}
+	if (isset($_POST['ORDERNUM2']))
+        {
+            // decalare varaiables
+	    $item_id2 = $_POST['ORDERNUM2'];
+	  	    
+            $prepared2 = $pdo->prepare('DELETE FROM Cart WHERE ORDERNUM = ?');
+
+	    // execute sql query
+	    $prepared2->execute(array($item_id2));
+	    //echo "<meta http-equiv='refresh' content='0'>";
+	}
+
 	echo "<br/>";
 	echo "<table cellpadding=30>";
 		echo "<tr>";
