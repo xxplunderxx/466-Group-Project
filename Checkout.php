0001309238e57232c006456131a05ee2201bd1da
doubleaa1 <89491959+doubleaa1@users.noreply.github.com>
2021-12-02 23:00:00 -0600

    Update Checkout.php

--- Checkout.php
+++ Checkout.php
@@ -13,6 +13,22 @@
         echo "<h1>";
         echo "CHECKOUT: ";
         echo "</h1>";
+                checkout_form();
+
+        if(isset($_POST['_NAME']))
+        {
+        // decalare varaiables
+        $name = $_POST['_NAME'];
+	    $addy = $_POST['ADDRESS'];
+	    $card = $_POST['CC'];
+           	     
+	    // insert new part
+            $prepared2 = $pdo->prepare('INSERT INTO User(_NAME,CC,ADDRESS) VALUE(?,?,?);');
+
+	    // execute sql query
+	    $prepared2->execute(array($name,$card,$addy));
+	    echo "<meta http-equiv='refresh' content='0'>";
+        }
         ?>
     </body>
 </html>
