8208f7ba6fd640890be6805b0b90a5683de50eb1
Justin Derus <77174869+Justin-Derus@users.noreply.github.com>
2021-12-03 20:00:21 -0600

    Update Fullfilment.php

--- Fullfilment.php
+++ Fullfilment.php
@@ -45,21 +45,21 @@
     }
 
     $sql = "SELECT QTY FROM Fullfilment;";
-	$result = $pdo->query($sql);
+    $result = $pdo->query($sql);
 
-	//FETCH BOTH NEEDED TO GET USABLE INDEX
-	$rows = $result->fetchAll(PDO::FETCH_BOTH);
-	$TOTAL = 0.00;
-	for($i = 0;$i < sizeof($rows);$i++){
-		for($j = 0; $j <1; $j++){
-			$TOTAL+=$rows[$i][$j];
-		}
-	}
-    	echo "<h2>";
-	echo "Total of order value: $".$TOTAL;
-	echo "</h2>";
-	echo "<br/>";
-
+    //FETCH BOTH NEEDED TO GET USABLE INDEX
+    $rows = $result->fetchAll(PDO::FETCH_BOTH);
+    $TOTAL = 0.00;
+    for($i = 0;$i < sizeof($rows);$i++){
+	 for($j = 0; $j <1; $j++){
+		$TOTAL+=$rows[$i][$j];
+	 }
+    }
+    echo "<h2>";
+    echo "Total of order value: $".$TOTAL;
+    echo "</h2>";
+    echo "<br/>";
+       
     // add notes 
     if(isset($_POST['Note']) AND isset($_POST['NUM']))
     {
@@ -68,13 +68,11 @@
         $prepared = $pdo->prepare('UPDATE Fullfilment SET NOTE = ? WHERE NUMBER = ?');
 
         // execute sql query
-        $prepared->execute(array($Note,$NUM));
+	$prepared->execute(array($Note,$NUM));
 	echo "<meta http-equiv='refresh' content='0'>";
 
         echo "You left a note on order ". $NUM. " Your note was ".$Note;
     }
-
-	?>
-
-</body>
+		?>
+	</body>
 </html>
