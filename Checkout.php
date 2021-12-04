17cf72a9425b5965fdb5a79078677fb00565a75d
Justin Derus <77174869+Justin-Derus@users.noreply.github.com>
2021-12-03 19:59:49 -0600

    Update Checkout.php
    working subtractions

--- Checkout.php
+++ Checkout.php
@@ -21,7 +21,7 @@
 		$TOTAL+= $total * $count;
 	}
 
-    echo "<h1>";
+    	echo "<h1>";
 	echo "CHECKOUT:";
 	echo "</h1>";
 	$result = $pdo->query("SELECT _NAME,_COUNT,TOTAL FROM Cart;");
@@ -35,32 +35,40 @@
 
 	checkout_form();
 
-    if(isset($_POST['_NAME']))
-    {
-        $name = $_POST['_NAME'];
-        $addy = $_POST['ADDRESS'];
-        $card = $_POST['CC'];
+   	if(isset($_POST['_NAME']))
+    	{
+            $name = $_POST['_NAME'];
+       	    $addy = $_POST['ADDRESS'];
+       	    $card = $_POST['CC'];
+            //working insert into User table	     
+            $prepared2 = $pdo->prepare('INSERT INTO User(_NAME,CC,ADDRESS) VALUE(?,?,?);');
+            $prepared2->execute(array($name,$card,$addy));
+            // echo "<meta http-equiv='refresh' content='0'>";
+    	}
 
-        //working insert into User table	     
-        $prepared2 = $pdo->prepare('INSERT INTO User(_NAME,CC,ADDRESS) VALUE(?,?,?);');
-        $prepared2->execute(array($name,$card,$addy));
-        echo "<meta http-equiv='refresh' content='0'>";
-    }
-
-	 if(isset($_POST['Done']))
+	if(isset($_POST['Done']))
         {
-        // insert into order
+            // insert into order
 	    $prepared2 = $pdo->prepare('INSERT INTO Ord(TOTAL) VALUE(?);');
 	    $prepared2->execute(array($TOTAL));
-
-        // insert into fullfilment
+            // insert into fullfilment
 	    $prepared2 = $pdo->prepare('INSERT INTO Fullfilment(_Status,QTY) VALUE(?,?);');
 	    $prepared2->execute(array('P',$TOTAL));
-
-	    echo "<meta http-equiv='refresh' content='0'>";
-	    //reset the shopping cart for the next customer
-        $result = $pdo->query("DELETE FROM Cart WHERE TOTAL > 0.00");
-	}
+	     echo "<meta http-equiv='refresh' content='0'>";
+	    //subtract how much the user bought from the Instock items
+	    $sql = "SELECT _NAME,_COUNT FROM Cart;";
+	    $result = $pdo->query($sql);	
+	    $rows = $result->fetchAll(PDO::FETCH_BOTH);
+	    //loop thru array to set the new quantities
+	    for($i = 0; $i < sizeof($rows); $i++){
+		    for($j = 1; $j<2;$j++){
+			    $prepared = $pdo->prepare('UPDATE Instock SET QTY = QTY - ? WHERE _NAME = ?;');
+			    $prepared->execute(array($rows[$i][$j],$rows[$i][0]));
+		    }
+	    }	   
+	    //reset shopping cart for next customer
+            $result = $pdo->query("DELETE FROM Cart WHERE TOTAL >= 0.00");
+	 }
 	?>
     </body>
 </html>
